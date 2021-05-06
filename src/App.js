7522045dfc6d14e60d549500b57131ab91cf532d
yvs2701 <yashvardhan.ys86@gmail.com>
2021-05-06 20:32:58 +0530

    All things up and working except Adding a todo

--- src/App.js
+++ src/App.js
@@ -2,38 +2,70 @@ import './App.css';
 import Header from "./header";
 import List from "./list";
 import Footer from "./footer";
+import Add from "./addTodo";
 
-function addTodo() {
-  let list = document.getElementsByClassName("list-group")[0];
-}
+import React, { Component, useState } from 'react'
 
-function App() {
-  let todos = [
-  {
-    title: "Insert your todo",
-    description: "Add a description if you may want",
-    // deadline: "By 12:00pm you gotta finish it !"
-  },
-  {
-    title: "Insert your todo",
-    description: "Add a description if you may want",
-    // deadline: "By 12:00pm you gotta finish it !"
-  },
-  {
-    title: "Insert your todo",
-    description: "Add a description if you may want",
-    // deadline: "By 12:00pm you gotta finish it !"
-  },
-] // array of objects
+export default function App() {
+  const [todos, setTodos] = useState([
+    {
+      title: "Insert your todo",
+      description: "Add a description if you may want",
+      // deadline: "By 12:00pm you gotta finish it !"
+    },
+    {
+      title: "Insert your todo",
+      description: "Add a description if you may want",
+      // deadline: "By 12:00pm you gotta finish it !"
+    },
+    {
+      title: "Insert your todo",
+      description: "Add a description if you may want",
+      // deadline: "By 12:00pm you gotta finish it !"
+    },
+  ] // array of objects
+  ); // here useState hook calls setTodos which will update our list
+
+  const addTodo = () => {
+    let text = document.getElementById("exampleFormControlInput1");
+    let descr = document.getElementById("exampleFormControlTextarea1");
+
+    if(text.value.trim() !== "")
+    {
+      let todoObj; // to store our new todo item
+      if(descr.value.trim() !== "")
+        todoObj = {
+          title: text.value().trim(),
+          description: descr.value().trim()
+        };
+      else
+        todoObj = {
+        title: text.value().trim(),
+        description: descr.value().trim()
+        };
+      setTodos([...todos, todoObj]);
+    }
+  };
+
+  const deleteTodo = (removeElem) => {
+    // in react this wont update the UI we need to use Hooks
+    // let index = todos.indexOf(removeElem);
+    // todos.splice(index, 1);
+
+    setTodos(todos.filter(
+      (elem) => {
+        return elem !== removeElem;
+      }));
+  };
+
   return (
     <div className="App">
       <Header />
-      <List todos={todos}/>
+      <Add new={addTodo}/>
+      <List todos={todos} del={deleteTodo} />
       <Footer />
     </div>
-  );
-  // here we passed a JS object todos as react property (props) argument 'todos'
-  // don't confuse yourself due to the same name
+    // here we passed a JS object todos as react property (props) argument 'todos'
+    // don't confuse yourself due to the same name
+  )
 }
-
-export default App;
